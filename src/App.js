008fb92b6d332023846896abded870622a8ee93d
Aubrey Jones <aubrey_jones07@hotmail.com>
2022-03-17 19:18:28 -0600

    update app.js route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {PortfolioContextProvider} from './contexts/PortfolioContext';
 import {Box, LinearProgress} from '@mui/material'; 
 import NetlifyIdentityContext from 'react-netlify-identity-gotrue';
 import NavBar from './Components/NavBar';
 import LandingPage from './Pages/Landing';
-import disneyvsnetflix from './Pages/DisneyVSNetflix';
+import DisneyVSNetflix from './Pages/DisneyVSNetflix';
 import catwalkco from './Pages/CatwalkCo'    
 //import Footer from './Components/Footer/Footer'; 
 import './App.css';
-//const LatestMeals = React.lazy(() => import('./Components/LatestMeals/LatestMeals'));
-//const MealDetails = React.lazy(() => import('./Components/MealDetails/MealDetails'));
-
 
 const style = {
  width: '100%'
@@ -29,7 +25,7 @@ const App = () => {
     </Box>}>
   <Switch>
   <Route path="/" exact component={LandingPage} />
-  <Route path="/disneyvsnetflix" exact component={disneyvsnetflix} />
+  <Route path="/disneyvsnetflix" exact component={DisneyVSNetflix} />
   <Route path="/catwalkco" exact component={catwalkco} />
   
   </Switch>
