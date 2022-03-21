72c8c4b335958e0c0a6c0f43fd96a63218635a47
Aubrey Jones <aubrey_jones07@hotmail.com>
2022-03-20 22:40:47 -0600

    update case study flow

--- src/Pages/CatwalkCo.js
+++ src/Pages/CatwalkCo.js
@@ -109,7 +109,7 @@ const typeStyles = {
 
 
 const designA = {
-  bgcolor: '#eee',
+  bgcolor: '#f2f8fb',
 
 }
 
@@ -121,9 +121,15 @@ color: '#30638E'
 }
 
 const paragraphA = {
-padding: '20px 70px 100px 70px',
+padding: '40px 70px',
 fontSize: '20px'
 }
+
+const titleA = {
+  padding: '20px 70px',
+  fontSize: '28px'
+  }
+  
 
 
 const cardStylesA = {
@@ -162,6 +168,15 @@ fontSize: '30px',
 color: '#30638E'
 
 }
+
+
+const titleB = {
+  padding: '40px 70px',
+  fontSize: '28px'
+  }
+
+
+
 
 const paragraphB = {
 padding: '20px 70px',
@@ -264,19 +279,19 @@ const drawerItemList = () => (
         <ListItemIcon sx={{color: "white"}}>
           <PollIcon/>
         </ListItemIcon>
-        <Link href="#researchsurvey" sx={linkStyling}>RESEARCH SURVEY</Link>
+        <Link href="#empathy" sx={linkStyling}>EMPATHY</Link>
       </ListItem>
       <ListItem button>
         <ListItemIcon sx={{color: "white"}}>
         <CompareIcon/> 
         </ListItemIcon>
-        <Link href="#researchsurvey" sx={linkStyling}>COMPETITOR ANALYSIS</Link>
+        <Link href="#define" sx={linkStyling}>DEFINE</Link>
       </ListItem>
       <ListItem button>
         <ListItemIcon sx={{color: "white"}}>
         <InsightsIcon/>
         </ListItemIcon>
-        <Link href="#userinsight" sx={linkStyling}>USER INSIGHT STATEMENT</Link>
+        <Link href="#ideate" sx={linkStyling}>IDEATE</Link>
       </ListItem>
 
 
@@ -284,76 +299,16 @@ const drawerItemList = () => (
         <ListItemIcon sx={{color: "white"}}>
         <PersonOutlineIcon/>
         </ListItemIcon>
-        <Link href="#protopersona" sx={linkStyling}>PROTO PERSONA</Link>
+        <Link href="#prototype" sx={linkStyling}>PROTOTYPE</Link>
       </ListItem>
   
       <ListItem button>
         <ListItemIcon sx={{color: "white"}}>
           <MovingIcon/>
         </ListItemIcon>
-        <Link href="#userjourneymap" sx={linkStyling}>USER JOURNEY MAP</Link>
+        <Link href="#test" sx={linkStyling}>TEST</Link>
       </ListItem>
   
-
-      <ListItem button>
-        <ListItemIcon sx={{color: "white"}}>
-          <StickyNote2Icon/>
-        </ListItemIcon>
-        <Link href="#affinitydiagram" sx={linkStyling}>AFFINITY DIAGRAM</Link>
-      </ListItem>
-      <ListItem button>
-        <ListItemIcon sx={{color: "white"}}>
-          <PriorityHighIcon/>
-        </ListItemIcon>
-        <Link href="#featureprioritizationmatrix" sx={linkStyling}>FEATURE PRIORITIZATION MATRIX</Link>
-      </ListItem>
-      <ListItem button>
-        <ListItemIcon sx={{color: "white"}}>
-          <AccountTreeIcon/>
-        </ListItemIcon>
-        <Link href="#sitemap" sx={linkStyling}>SITEMAP</Link>
-      </ListItem>
-      <ListItem button>
-        <ListItemIcon sx={{color: "white"}}>
-          <ModeIcon/>
-        </ListItemIcon>
-        <Link href="#sketches" sx={linkStyling}>SKETCHES</Link>
-      </ListItem>
-      <ListItem button>
-        <ListItemIcon sx={{color: "white"}}>
-          <ViewModuleIcon/>
-        </ListItemIcon>
-        <Link href="#lowfidelitywireframes" sx={linkStyling}>LOW-FIDELITY WIREFRAMES</Link>
-      </ListItem>
-
-      <ListItem button>
-        <ListItemIcon sx={{color: "white"}}>
-          <TextureIcon/>
-        </ListItemIcon>
-        <Link href="#patternlibrary" sx={linkStyling}>PATTERN LIBRARY</Link>
-      </ListItem>
-
-      <ListItem button>
-        <ListItemIcon sx={{color: "white"}}>
-          <ViewCompactIcon/>
-        </ListItemIcon>
-        <Link href="#hifidelitywireframes" sx={linkStyling}>HI-FIDELITY WIREFRAMES</Link>
-      </ListItem>
-
-      <ListItem button>
-        <ListItemIcon sx={{color: "white"}}>
-          <PhoneIphoneIcon/>
-        </ListItemIcon>
-        <Link href="#prototype" sx={linkStyling}>PROTOTYPE</Link>
-      </ListItem>
-
-      <ListItem button>
-        <ListItemIcon sx={{color: "white"}}>
-          <QuizIcon/>
-        </ListItemIcon>
-        <Link href="#usabilitytesting" sx={linkStyling}>USABILITY TESTING</Link>
-      </ListItem>
-    
 
       <ListItem button>
         <ListItemIcon sx={{color: "white"}}>
@@ -444,18 +399,55 @@ return (
 <Typography sx={headerA}>Challenge</Typography>
 <Typography sx={paragraphA}>While the world turned to remote work overnight, I wanted to build a communication bridge between designers, developers and 
 clients with an e-commerce platform website so that we can continue to develop our virtual worlds together in a timely fashion.</Typography>
-  
+<Typography sx={headerA}></Typography> 
   
 </Box> 
 
 
-{/*researchsurvey*/}
 
 
-<Box sx={designB} id="researchsurvey">
-<Typography sx={headerB}>Research Survey</Typography>
 
-<Typography sx={paragraphB}>I first needed to validate the idea of an e-commerce freelancing platform. I created a survey so that I could reach out to mulitple people online. I received 
+{/*empathy*/}
+
+
+
+{/*protopersona*/}
+
+
+<Box sx={designB} id="empathy">
+
+<Typography sx={headerB}>Empathy</Typography>
+
+<Typography sx={titleB}>Proto Persona</Typography>
+
+<Typography sx={paragraphB}>Meet Laurey Lane, a representative of our supposed audience. Does she fit the picture? Does she relate to our freelancers?</Typography>
+
+
+<Card sx={imageStyles}>
+      <CardMedia
+        component="img"
+        alt="Proto Persona"
+        image={ProtoPersona}
+       
+      />
+     
+     
+    </Card>
+
+    <Typography sx={paragraphB}></Typography>
+
+</Box>
+
+
+
+
+
+<Box sx={designB}>
+
+<Typography sx={titleB}>Research Survey</Typography>
+
+<Typography sx={paragraphB}>I first needed to validate the idea of an e-commerce freelancing platform and if interested parties such as Laurey would 
+find them useful. I created a survey so that I could reach out to mulitple people online. I received 
 an interesting mixed feedback for some of my results:</Typography>
 
 
@@ -475,7 +467,19 @@ an interesting mixed feedback for some of my results:</Typography>
     online marketing. Upwork is a freelancing website specifically for web developers and designers. </Typography>
     
     
-    <Typography sx={paragraphB}>So why do 33.3% of people use Upwork? How does it work for freelance? Does it have any popular competitors?</Typography>
+   
+    <Typography sx={paragraphB}>The study also gave me some insight as far as how users struggled with freelancing, no matter their preferred communication 
+    platform. Some direct quotes included:</Typography>
+    <Typography sx={quoteC}>1. "I wish I didn't have to set money aside for taxes."</Typography>
+    <Typography sx={quoteC}>2. "I wish I could change some employers perception of freelance work. One company I interviewed with last year 
+    told me if I accept their offer that I'd have to give up freelance work."</Typography>
+    <Typography sx={quoteC}>3. "Being able to not work weekends. Usually find myself with projects that go well into the weekend." </Typography>
+    <Typography sx={paragraphC}></Typography>
+
+
+    <Typography sx={paragraphB}>As the majority of my responders prefer word of mouth, could I persuade them to join the online community for 
+    freelancing if I focused on their frustrations and provided good solutions to them? Why do 33.3% of people use Upwork? How does it work for freelance? 
+    Does it have any popular competitors?</Typography>
 
 
 <Typography sx={paragraphB}></Typography>
@@ -489,13 +493,14 @@ an interesting mixed feedback for some of my results:</Typography>
 {/*competitoranalysis*/}
 
 
-<Box sx={designC} id="competitoranalysis">
-<Typography sx={headerC}>Competitor Analysis</Typography>
-<Typography sx={paragraphC}>I found a few direct competitors alongside Upwork for freelancers such as Fiverr Business and Behance. 
-I also realized that freelancers can connect to clients through indirect competitors such as LinkedIn and Indeed.</Typography>
+<Box sx={designB}>
+<Typography sx={titleB}>Competitor Analysis</Typography>
+<Typography sx={paragraphB}>I found a few direct competitors alongside Upwork for freelancers such as Fiverr Business and Behance. 
+I also realized that freelancers can connect to clients through indirect competitors such as LinkedIn and Indeed. I gathered intel 
+to know if these competitors provided solutions to freelancers in their problem areas. This is what I discovered:</Typography>
   
 
-<Card sx={cardStylesC}>
+<Card sx={cardStylesB}>
       <CardMedia
         component="img"
         alt="Competitor Analysis"
@@ -505,28 +510,28 @@ I also realized that freelancers can connect to clients through indirect competi
      
     </Card>
 
-    <Typography sx={paragraphC}>As the majority of my responders prefer word of mouth, could I persuade them to join the online community for 
-    freelancing?</Typography>
-    <Typography sx={paragraphC}>The study gave me some insight as far as how users struggled with freelancing, no matter their preferred communication 
-    platform. Some direct quotes included:</Typography>
-    <Typography sx={quoteC}>1. "I wish I didn't have to set money aside for taxes."</Typography>
-    <Typography sx={quoteC}>2. "I wish I could change some employers perception of freelance work. One company I interviewed with last year 
-    told me if I accept their offer that I'd have to give up freelance work."</Typography>
-    <Typography sx={quoteC}>3. "Being able to not work weekends. Usually find myself with projects that go well into the weekend." </Typography>
-    <Typography sx={paragraphC}></Typography>
+   
 </Box> 
 
 
 
-{/*userinsight*/}
 
-<Box sx={designB} id="userinsight">
-<Typography sx={headerB}>User Insight Statement</Typography>
 
-<Typography sx={paragraphB}>Freelancers and clients need to find a better online communication platform for freelancing because it is important to meet 
+
+
+
+
+{/*define*/}
+
+<Box sx={designA} id="define">
+<Typography sx={headerA}>Define</Typography>
+
+<Typography sx={titleA}>User Insight Statement</Typography>
+
+<Typography sx={paragraphA}>Freelancers and clients need to find a better online communication platform for freelancing because it is important to meet 
 project expectations without overwhelming anyone involved.</Typography>
     
-<Typography sx={paragraphB}></Typography>
+<Typography sx={paragraphA}></Typography>
   
 
 
@@ -535,38 +540,17 @@ project expectations without overwhelming anyone involved.</Typography>
 </Box> 
 
 
-{/*protopersona*/}
-
-
-<Box sx={designA} id="protopersona">
-<Typography sx={headerA}>Proto Persona</Typography>
-
-
-
-<Card sx={imageStyles}>
-      <CardMedia
-        component="img"
-        alt="Proto Persona"
-        image={ProtoPersona}
-       
-      />
-     
-     
-    </Card>
-
-    <Typography sx={paragraphA}></Typography>
-
-</Box>
 
 
 {/*journeymap*/}
 
 
 
-    <Box sx={designB} id="journeymap">
-<Typography sx={headerB}>Journey Map</Typography>
 
-<Typography sx={paragraphB}>Laurey Lane is a creative Beautician that likes to freelance for digital art. Here is her typical day:</Typography>
+<Box sx={designA} id="journeymap">
+<Typography sx={titleA}>Journey Map</Typography>
+
+<Typography sx={paragraphA}>Let's spend a moment with Laurey Lane as she expresses her typical day as a freelancer:</Typography>
 
 
 
@@ -581,7 +565,7 @@ project expectations without overwhelming anyone involved.</Typography>
      
     </Card>
 
-    <Typography sx={paragraphB}></Typography>
+    <Typography sx={paragraphA}></Typography>
    
     
 
@@ -591,12 +575,17 @@ project expectations without overwhelming anyone involved.</Typography>
 
 
 
+
+
+
 {/*Affinity Map*/}
 
 
 
-    <Box sx={designA} id="affinitydiagram">
-<Typography sx={headerA}>Affinity Diagram</Typography>
+    <Box sx={designA}>
+
+<Typography sx={titleA}>Affinity Diagram</Typography>
+
 
 <Typography sx={paragraphA}>With Laurey's freelancing experience in mind, 
 I used Miro to sort my user research data and organized its info into an affinity diagram. This lets me 
@@ -619,13 +608,13 @@ way as Laurey does about freelancing overall: </Typography>
 
 
 
-    <Typography sx={paragraphB}></Typography>
+    <Typography sx={paragraphA}></Typography>
     
     
-    <Typography sx={paragraphB}></Typography>
+    <Typography sx={paragraphA}></Typography>
 
 
-<Typography sx={paragraphB}></Typography>
+<Typography sx={paragraphA}></Typography>
 
 
 
@@ -637,17 +626,17 @@ way as Laurey does about freelancing overall: </Typography>
 {/*featureprioritizationmatrix*/}
 
 
-<Box sx={designB} id="featureprioritizationmatrix">
-<Typography sx={headerB}>Feature Priortization Matrix</Typography>
+<Box sx={designA}>
+<Typography sx={titleA}>Feature Priortization Matrix</Typography>
 
-<Typography sx={paragraphB}>I want to keep the most viable product (MVP) in mind as I don't want to create 
+<Typography sx={paragraphA}>I want to keep the most viable product (MVP) in mind as I don't want to create 
 any scope creep and be mindful of timing as I am on feature deadlines. I won't always have time to create 
 every feature request, which is okay. There can always be more time later on to create version two and testing each 
 step of the way will let me know if the product really truly has a worthy return on investment and meets my customers'
 needs. A Feature Prioritization Matrix lets me organize these requests based upon importance and urgency: </Typography>
 
 
-    <Card sx={cardStylesB}>
+    <Card sx={cardStylesA}>
       <CardMedia
         component="img"
         alt="Feature Prioritization Matrix"
@@ -660,7 +649,7 @@ needs. A Feature Prioritization Matrix lets me organize these requests based upo
 
 
 
-    <Typography sx={paragraphB}></Typography>
+    <Typography sx={paragraphA}></Typography>
     
     
 
