d9721cf368636b7be8837c66ca8d6dd4c66b3564
Aubrey Jones <aubrey_jones07@hotmail.com>
2022-04-09 20:15:06 -0600

    simplify app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {Box, LinearProgress} from '@mui/material'; 
+import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom'; 
 import NavBar from './Components/NavBar';
-import LandingPage from './Pages/Landing';
+import Landing from './Pages/Landing';
 import DisneyVSNetflix from './Pages/DisneyVSNetflix';
 import CatwalkCo from './Pages/CatwalkCo';     
 //import Footer from './Components/Footer/Footer'; 
 import './App.css';
 
-const style = {
- width: '100%'
-}
 
 
 const App = () => {
@@ -19,7 +15,7 @@ const App = () => {
    <Router>
      <NavBar />
 
-  <Route path="/" exact component={LandingPage} />
+  <Route path="/" exact component={Landing} />
   <Route path="/disneyvsnetflix" exact component={DisneyVSNetflix} />
   <Route path="/catwalkco" exact component={CatwalkCo} />
    </Router>
