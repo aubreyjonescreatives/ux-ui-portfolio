ddb7829d6198da278a2f75ca0e4612049e41974f
Aubrey Jones <aubrey_jones07@hotmail.com>
2023-06-02 22:00:13 -0600

    added logo images to carousel

--- src/Pages/LogoDesigns.js
+++ src/Pages/LogoDesigns.js
@@ -8,11 +8,17 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Carousel from 'react-bootstrap/Carousel';
-
+import '../App.css';
+import HelloMomentsFull from '../Images/logos/hellomomentslogo.png'; 
+import CatWalkCo from '../Images/logos/fulllogo-xl.png';
+import InstaFresh from '../Images/logos/instafresh.png';
 
 const carouselStyles = {
 clear: 'both', 
-marginTop: '100px'
+marginTop: '100px', 
+width: '500px', 
+height: '300px', 
+position: 'absolute'
 }
 
 
@@ -32,43 +38,27 @@ const LogoDesigns = () => {
       </Container>
     </Navbar>
 
-<Carousel sx={carouselStyles}>
+<Carousel sx={carouselStyles} classStyles="carouselStyles">
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
+          className="d-block carouselImage"
+          src={HelloMomentsFull}
           alt="First slide"
         />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
+          className="d-block carouselImage"
+          src={CatWalkCo}
           alt="Second slide"
         />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
+          className="d-block carouselImage"
+          src={InstaFresh}
           alt="Third slide"
         />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
 
