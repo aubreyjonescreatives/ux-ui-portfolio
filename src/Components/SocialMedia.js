dacf5ac85664b388d34d8fd9cb3332492020c7d5
Aubrey Jones <aubrey_jones07@hotmail.com>
2023-08-27 04:27:22 -0600

    adding consistency in bootstrap navbar

--- src/Components/SocialMedia.js
+++ src/Components/SocialMedia.js
@@ -1,5 +1,9 @@
 import React from 'react'
+import { Row, Col } from 'react-grid-system';
 import Box from '@mui/material/Box';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import Link from '@mui/material/Link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMedium, faBehanceSquare, faLinkedin,faSquareYoutube  } from '@fortawesome/free-brands-svg-icons' ;
@@ -33,8 +37,27 @@ const connectLink = {
 const SocialMedia = () => {
   return (
     <>
+ 
+        <Row>
+        <Nav.Link href="https://www.behance.net/aubreyjonesdesign" className="navLinkSocial">
+        <FontAwesomeIcon icon={faBehanceSquare} className="icon"/>
+        </Nav.Link>
+            <Nav.Link href="https://medium.com/@aubreyjonescreatives" className="navLinkSocial">
+            <FontAwesomeIcon icon={faMedium} className="icon"/>
+            </Nav.Link>
+            <Nav.Link href="https://www.linkedin.com/in/aubrey-jones" className="navLinkSocial">
+            <FontAwesomeIcon icon={faLinkedin} className="icon"/>
+            </Nav.Link>
+            <Nav.Link href="https://www.youtube.com/channel/UCw4Fi6cUbn67D18LqTPQymA" className="navLinkSocial">
+            <FontAwesomeIcon icon={faSquareYoutube} className="icon"/>
+            </Nav.Link>
+            </Row>
+          
 
-<Box sx={connectLinksBox}>
+
+
+
+{/* <Box sx={connectLinksBox}>
 
 <Link sx={connectLink} href="https://www.behance.net/aubreyjonesdesign" target="_blank">
 <FontAwesomeIcon icon={faBehanceSquare} className="icon"/>
@@ -56,7 +79,7 @@ const SocialMedia = () => {
 <FontAwesomeIcon icon={faLinkedin} className="icon"/>
 </Link>
 
-</Box>
+</Box> */}
    
     
     </>
