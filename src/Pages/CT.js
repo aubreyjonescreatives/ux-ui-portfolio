4fb58fdf4dcacc9c02fbb3db2d07c28c9d034a8c
Aubrey Jones <aubrey_jones07@hotmail.com>
2023-08-27 21:50:00 -0600

    updated ct ui case study

--- src/Pages/CT.js
+++ src/Pages/CT.js
@@ -195,9 +195,8 @@ const CT = () => {
 <Box sx={boxStylesB}>
   <Container>
 <Typography sx={headerStylesB}>Project Challenge</Typography>
-<Typography sx={paragraphA}>Landing International was working on a new website that built a bridge between beauty brands and products. 
-They became Codafi Technology's client after already completing the research phase to the UX/UI design process.
-They needed UX/UI designers to design and redesign specific features on their website.</Typography>
+<Typography sx={paragraphA}>Landing International needed UX/UI designers to update their website for beauty brands. This new update included 
+communication and navigation features between multiple points of view so that beauty brands and potential companies can communicate with each other.</Typography>
 </Container>
 <Container>
 <Typography sx={headerStylesB}>Project Constraints</Typography>
@@ -205,15 +204,14 @@ They needed UX/UI designers to design and redesign specific features on their we
 <Typography sx={paragraphA}>
   Client Expectations
 </Typography>
-<Typography sx={paragraphA}>Each new feature started out by a list of written requirements. Some of these concepts were difficult to 
-understand without more detail. We spent time with Landing International stakeholders to clearly identify their vision and goals well 
-before we spent time designing.
-to clearly </Typography>
+<Typography sx={paragraphA}>Each new feature started out by a list of written requirements and sometimes included a visual representation. 
+We spent time with Landing International stakeholders to clearly identify their vision and goals well before we spent time designing their wireframes.</Typography>
 <Typography sx={paragraphA}>
   Work Availability
 </Typography>
 <Typography sx={paragraphA}>The work load carried a lot of inconsistency. Sometimes features did not take as long as others and sometimes 
-we were out of work altogether.</Typography>
+we were out of work altogether. To resolve this issue, we excercised patience and direct communication to see if our client needed any additional 
+work throughout the project.</Typography>
 
 
 
@@ -270,7 +268,7 @@ we were out of work altogether.</Typography>
 
 <Container>
 
-<Typography sx={sectiontitleB}>Top Banner Redesign</Typography>
+<Typography sx={sectiontitleB}>Top Banner Redesign Example</Typography>
 
 <Row>
 
@@ -279,7 +277,7 @@ we were out of work altogether.</Typography>
 <Card sx={imageStylesB}>
       <CardMedia
         component="img"
-        alt="Original Algebra 1 program"
+        alt="Top Banner Redesign"
         image={BeforeandAfterHero}
        
       />
@@ -299,7 +297,7 @@ we were out of work altogether.</Typography>
  2. I fixed alignment issues for better readability.
 </Typography >
 <Typography sx={paragraphB}>
-  3. Added a call-to-action button for navigation accessibility.
+  3. Landing International requested a call-to-action button for navigation accessibility.
 </Typography>
 </Col>
 
@@ -321,80 +319,24 @@ we were out of work altogether.</Typography>
 
 
 
+
 <Box sx={boxStylesB}>
 
 <Container>
-
-<Typography sx={headerStylesB}>Algebra 1 Lesson Plan Redesign</Typography>
-
-<Typography sx={paragraphB}>The Algebra 1 program contained 15 modules and 4 - 12 lessons in each module. Discovering a user-friendly 
-layout included quite a few different needs listed on the left. The Family Literacy Centers offered an internship 
-opportunity to continue the redesign. While my teammates had other summer plans, I continued working on the redesign with UI design principles 
-in mind. We also brought on Erica, a content editor, as she noticed some grammar, spelling, and punctuation mistakes made througout the 
-original lessons.</Typography>
-
-
-<Typography sx={sectiontitleB}>Key Design Changes and Rationale:</Typography>
+<Typography sx={headerStylesB}>Project Key Findings</Typography>
 
 <Row>
 <Col>
 <Typography sx={paragraphB}>
- 1. Calmer colors out of their style guide for better readability.
+ 1. Each wireframe required pixel-to-pixel perfection with UI design principles such as alignment, whitespace, chunking, etc.
 </Typography>
 <Typography sx={paragraphB}>
-  2. Clearer top navigation for lesson location.
+ 2. Exact wants and needs happened through verbal and written communication. 
 </Typography>
 <Typography sx={paragraphB}>
-  3. Left and right buttons originally drew too much attention. We wanted our students to focus on 
-  page content instead. These buttons only appear as accessible on hover, when the student is ready 
-  to click on them.
-</Typography >
-<Typography sx={paragraphB}>
-  4. Visual progress for clarity to avoid the "Are we there yet?" question. 
+ 3. I gained more Figma practice throughout this redesign.
 </Typography>
-</Col>
 
-<Col>
-
-<Card sx={imageStylesB}>
-      <CardMedia
-        component="img"
-        alt="New Algebra 1 Program"
-        image={BeforeandAfterHero}
-       
-      />
-     
-     
-    </Card>
-
-</Col>
-
-
-</Row>
-</Container>
-
-</Box> 
-
-
-<Box sx={boxStylesA}>
-
-<Container>
-<Typography sx={headerStylesA}>Project Key Findings</Typography>
-
-<Row>
-<Col>
-<Typography sx={paragraphB}>
- 1. We could resolve the first quest to update Flash files into current HTML5 practices.
-</Typography>
-<Typography sx={paragraphB}>
-  2. The information architecture needed more clarity with mental model placements and modern color choices.
-</Typography>
-<Typography sx={paragraphB}>
-  3. Some of the lessons had poor grammar and spelling mistakes. We brought on an editor to help fix these issues.
-</Typography >
-<Typography sx={paragraphB}>
-  4. Updating the lesson plans provided more opportunity for students to engage in modern content. 
-</Typography>
 </Col>
 
 
