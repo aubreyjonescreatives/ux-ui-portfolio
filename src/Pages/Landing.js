8c1cd7d3804cef4adc05f2dac3a7154b994c62d8
Aubrey Jones <aubrey_jones07@hotmail.com>
2023-08-29 23:37:49 -0600

    removed extra

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -88,7 +88,6 @@ const cardStyles = {
 margin: '20px auto', 
 textDecoration: 'none',
 fontFamily: 'Raleway, sans-serif', 
-border: 'none', 
 boxShadow: 'none',
 borderRadius: '15px',
 backgroundColor: '#FFF', 
