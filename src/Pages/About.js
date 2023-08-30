3677308c2da29c0351a188b448166ec3f0fc7d32
Aubrey Jones <aubrey_jones07@hotmail.com>
2023-08-29 23:48:38 -0600

    edit

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -171,7 +171,7 @@ const About = () => {
    <Box sx={boxStylesAFirst}>
     <Container>
    <Typography sx={headerStylesAFirst}>About Me</Typography>
-   <Typography sx={headerStylesAFirst}>I'm a creative UX/UI Designer based in Salt Lake City, Utah, since 2016.</Typography> 
+   <Typography sx={headerStylesAFirst}>I am a creative UX/UI Designer based in Salt Lake City, Utah, since 2016.</Typography> 
 
    </Container>
    </Box>
